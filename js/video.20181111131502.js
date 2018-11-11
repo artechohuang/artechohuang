7c11c0927ae51eef8f34941339e3f18a8a1bdaca
黄雅琪 <art_huangyaqi@huangyaqideMacBook-Pro.local>
2018-11-11 13:16:22 +0800

    1

---
+++ js/video.20181111131502.js
@@ -0,0 +1,88 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+var lwi=-1;function thresholdPassed(){var w=$(window).width();var p=false;var cw=0;if(w>=1200){cw++;}if(lwi!=cw){p=true;}lwi=cw;return p;}
+
+$(document).ready(function() {
+r=function(){if(thresholdPassed()){dpi=window.devicePixelRatio;if($(window).width()>=1200){var a='data-src'; if($('.js-551 .slide0').hasAttr('src')) { a='src'; } $('.js-551 .slide0').attr(a, (dpi>1) ? 'images/69646ccagy1fd1phvpw65j22gi1w0hdt-2400-1.jpg' : 'images/69646ccagy1fd1phvpw65j22gi1w0hdt-1200-1.jpg');
+var a='data-src'; if($('.js-551 .slide1').hasAttr('src')) { a='src'; } $('.js-551 .slide1').attr(a, (dpi>1) ? 'images/psb-2400-1.jpg' : 'images/psb-1200-1.jpg');
+var a='data-src'; if($('.js-551 .slide2').hasAttr('src')) { a='src'; } $('.js-551 .slide2').attr(a, (dpi>1) ? 'images/wechatimg24-2400.jpg' : 'images/wechatimg24-1200.jpg');
+var a='data-src'; if($('.js-551 .slide3').hasAttr('src')) { a='src'; } $('.js-551 .slide3').attr(a, (dpi>1) ? 'images/wechatimg680-2-2400.jpg' : 'images/wechatimg680-2-1200.jpg');
+var a='data-src'; if($('.js-551 .slide4').hasAttr('src')) { a='src'; } $('.js-551 .slide4').attr(a, (dpi>1) ? 'images/wechatimg739-2400.jpg' : 'images/wechatimg739-1200.jpg');
+var a='data-src'; if($('.js-551 .slide5').hasAttr('src')) { a='src'; } $('.js-551 .slide5').attr(a, (dpi>1) ? 'images/211-2400.jpg' : 'images/211-1200.jpg');
+var a='data-src'; if($('.js-551 .slide6').hasAttr('src')) { a='src'; } $('.js-551 .slide6').attr(a, (dpi>1) ? 'images/wechatimg49-2400-1.jpg' : 'images/wechatimg49-1200-1.jpg');
+var a='data-src'; if($('.js-551 .slide7').hasAttr('src')) { a='src'; } $('.js-551 .slide7').attr(a, (dpi>1) ? 'images/311-2400.jpg' : 'images/311-1200.jpg');
+var a='data-src'; if($('.js-551 .slide8').hasAttr('src')) { a='src'; } $('.js-551 .slide8').attr(a, (dpi>1) ? 'images/1-2400-1.jpg' : 'images/1-1200-1.jpg');
+var a='data-src'; if($('.js-551 .slide9').hasAttr('src')) { a='src'; } $('.js-551 .slide9').attr(a, (dpi>1) ? 'images/rg-2400.jpg' : 'images/rg-1200.jpg');
+var a='data-src'; if($('.js-551 .slide10').hasAttr('src')) { a='src'; } $('.js-551 .slide10').attr(a, (dpi>1) ? 'images/wechatimg23021313-2400.jpg' : 'images/wechatimg23021313-1200.jpg');
+var a='data-src'; if($('.js-551 .slide11').hasAttr('src')) { a='src'; } $('.js-551 .slide11').attr(a, (dpi>1) ? 'images/wechatimg551-2-2400-1.jpg' : 'images/wechatimg551-2-1200-1.jpg');
+$('.js-552').attr('src', (dpi>1) ? 'images/echo-huang-780.png' : 'images/echo-huang-390.png');
+$('.js-553').attr('src', (dpi>1) ? 'images/echo-huang-780-2.png' : 'images/echo-huang-390-2.png');
+$('.js-554').attr('src', (dpi>1) ? 'images/echo-huang-780.png' : 'images/echo-huang-390.png');
+$('.js-555').attr('src', (dpi>1) ? 'images/soutu123.com_4217226_xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-2-46.png' : 'images/soutu123.com_4217226_xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-2-23.png');
+$('.js-556').attr('src', (dpi>1) ? 'images/soutu123.com_4217226_xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-2-46.png' : 'images/soutu123.com_4217226_xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-2-23.png');
+$('.js-557').attr('src', (dpi>1) ? 'images/facebook29-copy-38.png' : 'images/facebook29-copy-19.png');
+$('.js-558').attr('src', (dpi>1) ? 'images/facebook29-copy-38.png' : 'images/facebook29-copy-19.png');
+$('.js-559').attr('src', (dpi>1) ? 'images/u-2260898377-2056122756-fm-26-gp-0-copy-40.png' : 'images/u-2260898377-2056122756-fm-26-gp-0-copy-20.png');
+$('.js-560').attr('src', (dpi>1) ? 'images/u-2260898377-2056122756-fm-26-gp-0-copy-40.png' : 'images/u-2260898377-2056122756-fm-26-gp-0-copy-20.png');
+$('.js-561').attr('src', (dpi>1) ? 'images/u-2260898377-2056122756-fm-26-gp-0-copy-40.png' : 'images/u-2260898377-2056122756-fm-26-gp-0-copy-20.png');
+$('.js-562').attr('src', 'images/ju-xing-copy-2-1189.png');
+$('.js-563').attr('src', 'images/zhi-xian-71.png');
+$('.js-564').attr('src', (dpi>1) ? 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-46.png' : 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-23.png');
+$('.js-565').attr('src', (dpi>1) ? 'images/facebook29-38.png' : 'images/facebook29-19.png');
+$('.js-566').attr('src', (dpi>1) ? 'images/instagram-2-40.png' : 'images/instagram-2-20.png');
+$('.js-568').attr('src', (dpi>1) ? 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-46.png' : 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-23.png');
+$('.js-569').attr('src', (dpi>1) ? 'images/facebook29-38.png' : 'images/facebook29-19.png');
+$('.js-570').attr('src', (dpi>1) ? 'images/instagram-2-40.png' : 'images/instagram-2-20.png');
+$('.js-571').attr('src', 'images/ju-xing-1200-2.png');
+$('.js-573').attr('src', 'images/biography-529.jpg');
+$('.js-574').attr('src', (dpi>1) ? 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-46.png' : 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-23.png');
+$('.js-575').attr('src', (dpi>1) ? 'images/facebook29-38.png' : 'images/facebook29-19.png');
+$('.js-576').attr('src', (dpi>1) ? 'images/instagram-2-40.png' : 'images/instagram-2-20.png');
+$('.js-577').attr('src', (dpi>1) ? 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-46.png' : 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-23.png');
+$('.js-578').attr('src', (dpi>1) ? 'images/facebook29-38.png' : 'images/facebook29-19.png');
+$('.js-579').attr('src', (dpi>1) ? 'images/instagram-2-40.png' : 'images/instagram-2-20.png');
+$('.js-580').attr('src', (dpi>1) ? 'images/xin-lang-wei-bo-biao-zhi-tu-biao_-copy-4-140.png' : 'images/xin-lang-wei-bo-biao-zhi-tu-biao_-copy-4-70.png');
+$('.js-581').attr('src', (dpi>1) ? 'images/xin-lang-wei-bo-biao-zhi-tu-biao_-copy-4-140.png' : 'images/xin-lang-wei-bo-biao-zhi-tu-biao_-copy-4-70.png');
+$('.js-582').attr('src', (dpi>1) ? 'images/facebook29-copy-3-176.png' : 'images/facebook29-copy-3-88.png');
+$('.js-583').attr('src', (dpi>1) ? 'images/facebook29-copy-3-176.png' : 'images/facebook29-copy-3-88.png');
+$('.js-584').attr('src', (dpi>1) ? 'images/u-2260898377-2056122756-fm-26-gp-0-copy-5-184.png' : 'images/u-2260898377-2056122756-fm-26-gp-0-copy-5-92.png');
+$('.js-585').attr('src', (dpi>1) ? 'images/u-2260898377-2056122756-fm-26-gp-0-copy-5-184.png' : 'images/u-2260898377-2056122756-fm-26-gp-0-copy-5-92.png');}else{$('.js-552').attr('src', (dpi>1) ? 'images/echo-huang-318.png' : 'images/echo-huang-159.png');
+$('.js-553').attr('src', (dpi>1) ? 'images/echo-huang-208.png' : 'images/echo-huang-104.png');
+$('.js-554').attr('src', (dpi>1) ? 'images/echo-huang-318.png' : 'images/echo-huang-159.png');
+$('.js-555').attr('src', (dpi>1) ? 'images/soutu123.com_4217226_xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-2-38.png' : 'images/soutu123.com_4217226_xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-2-19.png');
+$('.js-556').attr('src', 'images/soutu123.com_4217226_xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-2-47.png');
+$('.js-557').attr('src', (dpi>1) ? 'images/facebook29-copy-34.png' : 'images/facebook29-copy-17.png');
+$('.js-558').attr('src', 'images/facebook29-copy-41.png');
+$('.js-559').attr('src', (dpi>1) ? 'images/u-2260898377-2056122756-fm-26-gp-0-copy-34.png' : 'images/u-2260898377-2056122756-fm-26-gp-0-copy-17.png');
+$('.js-560').attr('src', 'images/u-2260898377-2056122756-fm-26-gp-0-copy-41.png');
+$('.js-561').attr('src', 'images/u-2260898377-2056122756-fm-26-gp-0-copy-41.png');
+$('.js-562').attr('src', (dpi>1) ? 'images/ju-xing-copy-2-640.png' : 'images/ju-xing-copy-2-320.png');
+$('.js-563').attr('src', (dpi>1) ? 'images/zhi-xian-26.png' : 'images/zhi-xian-13.png');
+$('.js-564').attr('src', (dpi>1) ? 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-36.png' : 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-18.png');
+$('.js-565').attr('src', (dpi>1) ? 'images/facebook29-32.png' : 'images/facebook29-16.png');
+$('.js-566').attr('src', (dpi>1) ? 'images/instagram-2-32.png' : 'images/instagram-2-16.png');
+$('.js-568').attr('src', (dpi>1) ? 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-36.png' : 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-18.png');
+$('.js-569').attr('src', (dpi>1) ? 'images/facebook29-32.png' : 'images/facebook29-16.png');
+$('.js-570').attr('src', (dpi>1) ? 'images/instagram-2-32.png' : 'images/instagram-2-16.png');
+$('.js-571').attr('src', (dpi>1) ? 'images/ju-xing-640-1.png' : 'images/ju-xing-320-1.png');
+$('.js-573').attr('src', (dpi>1) ? 'images/biography-640.jpg' : 'images/biography-320.jpg');
+$('.js-574').attr('src', (dpi>1) ? 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-36.png' : 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-18.png');
+$('.js-575').attr('src', (dpi>1) ? 'images/facebook29-32.png' : 'images/facebook29-16.png');
+$('.js-576').attr('src', (dpi>1) ? 'images/instagram-2-32.png' : 'images/instagram-2-16.png');
+$('.js-577').attr('src', (dpi>1) ? 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-36.png' : 'images/xin-lang-wei-bo-biao-zhi-tu-biao_sou-tu123zhu-nin-gong-zuo-shun-li-copy-18.png');
+$('.js-578').attr('src', (dpi>1) ? 'images/facebook29-32.png' : 'images/facebook29-16.png');
+$('.js-579').attr('src', (dpi>1) ? 'images/instagram-2-32.png' : 'images/instagram-2-16.png');
+$('.js-580').attr('src', (dpi>1) ? 'images/xin-lang-wei-bo-biao-zhi-tu-biao_-copy-4-106.png' : 'images/xin-lang-wei-bo-biao-zhi-tu-biao_-copy-4-53.png');
+$('.js-581').attr('src', (dpi>1) ? 'images/xin-lang-wei-bo-biao-zhi-tu-biao_-copy-4-106.png' : 'images/xin-lang-wei-bo-biao-zhi-tu-biao_-copy-4-53.png');
+$('.js-582').attr('src', (dpi>1) ? 'images/facebook29-copy-3-106.png' : 'images/facebook29-copy-3-53.png');
+$('.js-583').attr('src', (dpi>1) ? 'images/facebook29-copy-3-106.png' : 'images/facebook29-copy-3-53.png');
+$('.js-584').attr('src', (dpi>1) ? 'images/u-2260898377-2056122756-fm-26-gp-0-copy-5-106.png' : 'images/u-2260898377-2056122756-fm-26-gp-0-copy-5-53.png');
+$('.js-585').attr('src', (dpi>1) ? 'images/u-2260898377-2056122756-fm-26-gp-0-copy-5-106.png' : 'images/u-2260898377-2056122756-fm-26-gp-0-copy-5-53.png');}}};
+if(!window.HTMLPictureElement){$(window).resize(r);r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+plyr.setup(document.querySelectorAll('.player video'));
+if($('.js-567 video')[0].plyr.volume){$('.js-567 video')[0].plyr.setVolume(8);}
+if($('.js-572 video')[0].plyr.volume){$('.js-572 video')[0].plyr.setVolume(8);}
+$('.js-551 .slider').slick({lazyLoad: 'ondemand',slidesToShow: 1,slidesToScroll: 1,fade: true,slide: 'div',cssEase: 'linear',speed: 1050,dots: false,arrows: false,infinite: true,autoplay: true,pauseOnHover: false,autoplaySpeed: 2450});
+});
